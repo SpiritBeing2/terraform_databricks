f938970adedfe87d8e0955a2b0133d6c1639f26a
SpiritBeing2 <kankersan@gmail.com>
2026-06-11 01:37:54 +0200

    add int

---
+++ envs/int/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "terraform"
+    storage_account_name = "statesterraform1996"
+    container_name       = "terraform"
+    key                  = "int.terraform.state"
+  }
+}
--- envs/int/main.tf
+++ envs/int/main.tf
@@ -4,4 +4,9 @@ module "platform" {
   location            = var.location
   environment         = var.environment
   resource_group_name = var.resource_group_name
+
+  resource_group_vault = var.resource_group_vault
+  vault_name = var.vault_name
+
 }
+
